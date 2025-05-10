e935e05179c827ef99919bdec172a38480e813b2
Valentin Churavy <v.churavy@gmail.com>
2025-05-10 23:07:46 +0200

    add test against taylor_diff up to 5th order

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,19 @@ using TowerOfEnzyme
 using StaticArrays
 import TaylorDiff
 
+# c.f. https://github.com/JuliaDiff/TaylorDiff.jl/issues/98
+# Internally TaylorDiff is storing Taylor polynomial coefficients, i.e. d^n u / n! , instead of derivatives.
+# Therefore, when you convert from derivatives to coefficients, divide by n!, and when you convert from coefficients to derivative, multiply by n!.
+# The helper function TaylorDiff.extract_derivative already handle the latter for you, but the former is currently missing.
+function taylor_diff(f::F, bundle::NTuple{N, T}) where {F, N, T}
+    coeffs = ntuple(Val(N - 1)) do i
+        1 // factorial(i) * bundle[i + 1]
+    end
+    taylor_bundle = map((x, dx...) -> TaylorDiff.TaylorScalar(x, dx), bundle[1], coeffs...)
+    f_bundle = f(taylor_bundle)
+    return TaylorDiff.extract_derivative(f_bundle, Val(N - 1))
+end
+
 @testset "sin" begin
     @test nth_derivative(sin, 1.0, Val(1)) ≈ cos(1.0)
     @test nth_derivative(sin, 1.0, Val(2)) ≈ -sin(1.0)
@@ -30,5 +43,11 @@ end
     @test TaylorDiff.derivative(flux, u, du, Val(1)) ≈ jvp(flux, u, du)
     @test TaylorDiff.derivative(flux, u, du, Val(2)) ≈ hvvp(flux, u, du)
 
+    @test derivative_bundle(flux, (u, du)) ≈ jvp(flux, u, du)
     @test derivative_bundle(flux, (u, du, ddu)) ≈ hvvp(flux, u, du) + jvp(flux, u, ddu)
+
+    @test derivative_bundle(flux, (u, du)) ≈ taylor_diff(flux, (u, du))
+    @test derivative_bundle(flux, (u, du, ddu)) ≈ taylor_diff(flux, (u, du, ddu))
+    @test derivative_bundle(flux, (u, du, ddu, dddu)) ≈ taylor_diff(flux, (u, du, ddu, dddu))
+    @test derivative_bundle(flux, (u, du, ddu, dddu, ddddu)) ≈ taylor_diff(flux, (u, du, ddu, dddu, ddddu))
 end
