e8a9bcee463aa72fc4bb22964fd746c81a54a594
Valentin Churavy <v.churavy@gmail.com>
2025-05-16 11:08:59 +0200

    add comparison with TaylorDiff and ForwardDiff (#4)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,10 +1,31 @@
 using BenchmarkTools
 using TowerOfEnzyme
 
+import ForwardDiff
+import TaylorDiff
+
+function nth_derivative_forwarddiff(f::F, x, ::Val{N}) where {F, N}
+   if N == 0
+	  return f(x)
+   else
+	  return ForwardDiff.derivative(y->nth_derivative_forwarddiff(f, y, Val(N-1)), x)
+   end
+end
+
 const SUITE = BenchmarkGroup()
 
 SUITE["sin"] = BenchmarkGroup()
-SUITE["sin"]["overhead"] = @benchmarkable nth_derivative(sin, 1.0, Val(0))
-for order in (1, 2, 4, 8, 13)
-    SUITE["sin"]["order = $order"] = @benchmarkable nth_derivative(sin, 1.0, Val($order))
+SUITE["sin"]["primal"] = sin($(Ref(1.0))[])
+SUITE["sin"]["TowerOfEnzyme"] =  BenchmarkGroup()
+SUITE["sin"]["ForwardDiff"] =  BenchmarkGroup()
+SUITE["sin"]["TaylorDiff"] =  BenchmarkGroup()
+
+function create_benchmark_sin(group, nth_derivative)
+    for order in (1, 2, 4, 8, 13)
+        group["order = $order"] = @benchmarkable $(nth_derivative)(sin, $(Ref(1.0))[], Val($order))
+    end
 end
+
+create_benchmark_sin(SUITE["sin"]["TowerOfEnzyme"], nth_derivative)
+create_benchmark_sin(SUITE["sin"]["ForwardDiff"], nth_derivative_forwarddiff)
+create_benchmark_sin(SUITE["sin"]["TowerOfEnzyme"], TaylorDiff.derivative)
