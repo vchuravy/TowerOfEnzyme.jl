96ffb1cfed3e2c0a36a80ff15c8e372ba87136a9
Valentin Churavy <v.churavy@gmail.com>
2025-05-16 09:54:44 +0200

    Add higher order benchmark (#2)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,6 +3,8 @@ using TowerOfEnzyme
 
 const SUITE = BenchmarkGroup()
 
-SUITE["basics"] = BenchmarkGroup()
-
-SUITE["basics"]["overhead"] = @benchmarkable nth_derivative(sin, 1.0, Val(0))
+SUITE["sin"] = BenchmarkGroup()
+SUITE["sin"]["overhead"] = @benchmarkable nth_derivative(sin, 1.0, Val(0))
+for order in (1, 2, 4, 8, 13)
+    SUITE["sin"]["order = $order"] = @benchmarkable nth_derivative(sin, 1.0, Val(order))
+end
