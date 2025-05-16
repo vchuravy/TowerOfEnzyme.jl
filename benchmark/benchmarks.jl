fec89fd62ce56b7f934d1e157852b69f82cb8101
Valentin Churavy <v.churavy@gmail.com>
2025-05-16 10:28:03 +0200

    Fix benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,5 +6,5 @@ const SUITE = BenchmarkGroup()
 SUITE["sin"] = BenchmarkGroup()
 SUITE["sin"]["overhead"] = @benchmarkable nth_derivative(sin, 1.0, Val(0))
 for order in (1, 2, 4, 8, 13)
-    SUITE["sin"]["order = $order"] = @benchmarkable nth_derivative(sin, 1.0, Val(order))
+    SUITE["sin"]["order = $order"] = @benchmarkable nth_derivative(sin, 1.0, Val($order))
 end
