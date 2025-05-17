b6ac90cb7cbf43c91f6e44ee939e46298dc1df2a
Valentin Churavy <v.churavy@gmail.com>
2025-05-17 11:22:17 +0200

    improve docs and cleanup questionable functions

--- src/TowerOfEnzyme.jl
+++ src/TowerOfEnzyme.jl
@@ -6,15 +6,18 @@ export nth_derivative, jvp, hvvp
 export derivative_bundle
 
 """
+    nth_derivative(f, x, Val(Order))
     nth_derivative(f, x, dx, Val(Order))
+
+Calculates the `Order`-th directional derivative of `f` with regards to `x` in direction `dx`.
+If `x` is a scalar `dx` can be omitted and is set to `one(x)`.
 """
+function nth_derivative end
+
 function nth_derivative(f::F, x::T, ::Val{O}) where {F, O, T}
     return nth_derivative(f, x, one(x), Val(O))
 end
 
-"""
-    nth_derivative(f, x, dx, Val(Order))
-"""
 function nth_derivative(f::F, x::T, dx::T, ::Val{O}) where {F, O, T}
     if O == 0
         return f(x)
@@ -26,23 +29,6 @@ function nth_derivative(f::F, x::T, dx::T, ::Val{O}) where {F, O, T}
         ) |> only
     end
 end
-
-"""
-    nth_derivative(f, x, (dx...))
-"""
-function nth_derivative(f::F, x::T, dx::NTuple{N, T}, ::Val{O}) where {F, N, T, O}
-    if O == 0
-        return f(x)
-    else
-        return Enzyme.autodiff(
-            Enzyme.set_abi(Forward, Enzyme.InlineABI),
-            nth_derivative, Const(f),
-            Duplicated(x, dx[O]), Const(dx), Const(Val(O - 1))
-        ) |> only
-    end
-end
-
-nth_derivative(f::F, x::T, dx::NTuple{N, T}) where {F, N, T} = nth_derivative(f, x, dx, Val(N))
 
 # Helper functions
 """
@@ -60,14 +46,9 @@ Calculate the Hessian-Vector-Vector product ``H_f(x) * v * v``
 hvvp(f, x, v) = nth_derivative(f, x, v, Val(2))
 
 """
-    hvvp(f, x, v₁, v₂)
-
-Calculate the Hessian-Vector-Vector product ``H_f(x) * v₁ * v₂``
-"""
-hvvp(f, x, v₁, v₂) = nth_derivative(f, x, (v₁, v₂), Val(2))
-
-"""
     derivative_bundle(f, (x, dx, ddx...))
+
+Calculates the higher-order pushforward.
 """
 @inline function derivative_bundle(f::F, bundle::NTuple{N}) where {F, N}
     if length(bundle) == 1
